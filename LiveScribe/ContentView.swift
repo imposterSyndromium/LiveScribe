a8ef3516bb6fd57c325739d5c5d19fd5884f7f80
Robin OBrien <robin.c.obrien@gmail.com>
2025-02-09 15:26:30 -0500

    Initial Commit

---
+++ LiveScribe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveScribe
+//
+//  Created by Robin O'Brien on 2025-02-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveScribe/LiveScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveScribeApp.swift
+//  LiveScribe
+//
+//  Created by Robin O'Brien on 2025-02-09.
+//
+
+import SwiftUI
+
+@main
+struct LiveScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
